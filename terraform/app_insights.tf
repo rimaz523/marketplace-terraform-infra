7df997a55314e3c4968ea0c4b74d4d6314cfef57
Mohommed Rimaz <rimazmohommed523@gmail.com>
2023-06-25 09:35:19 +0000

    add application insights resource (#10)
    * set allow tracing to false for apim subs
    
    * add application insights resource

---
+++ terraform/app_insights.tf
@@ -0,0 +1,17 @@
+module "app_insights" {
+  source   = "./modules/monitor/application_insights"
+  for_each = var.app_insights
+
+  resource_group_name = module.resource_group["common"].name
+  location            = module.resource_group["common"].location
+  name                = each.key
+  application_type    = each.value.application_type
+  retention_days      = each.value.retention_days
+  workspace_id        = module.log_analytics["app"].id
+  project             = var.project
+
+  depends_on = [
+    module.resource_group["common"].id,
+    module.log_analytics["app"].id
+  ]
+}
---
+++ terraform/modules/monitor/log_analytics_workspace/variables.tf
@@ -0,0 +1,29 @@
+variable "resource_group_name" {
+  description = "Name of the resource group where the log analytics workspace should reside in."
+  type        = string
+}
+
+variable "location" {
+  description = "Location of the log analytics workspace."
+  type        = string
+}
+
+variable "name" {
+  description = "Name of the log analytics workspace."
+  type        = string
+}
+
+variable "sku" {
+  description = "SKU of the log analytics workspace."
+  type        = string
+}
+
+variable "retention_days" {
+  description = "Number of days to retain logs."
+  type        = string
+}
+
+variable "project" {
+  description = "Name of the project."
+  type        = string
+}
