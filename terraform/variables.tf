75b72d24fbbc201ea98a7a6688a871388bbaaafc
Mohommed Rimaz <rimazmohommed523@gmail.com>
2023-07-03 14:29:56 +1200

    add ai keys and conn strings to app kv

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -186,3 +186,39 @@ variable "key_vaults" {
     }
   }
 }
+
+variable "app_key_vault_secrets" {
+  description = "create key vault resources"
+  type = map(object({
+    secret = string
+  }))
+  default = {
+    "application-insights-key" = {
+      secret = "instrumentation_key"
+    }
+    "application-insights-conn-string" = {
+      secret = "connection_string"
+    }
+  }
+}
+
+variable "app_key_vault_rbac" {
+  description = "RBAC role assignments for app key vault"
+  type = map(object({
+    roles                            = list(string)
+    principal_id                     = string
+    skip_service_principal_aad_check = bool
+  }))
+  default = {
+    "app_kv_mkt_principal" = {
+      "principal_id"                   = "#{devops_service_principal_id}#"
+      "roles"                          = ["Owner", "Key Vault Secrets Officer"]
+      skip_service_principal_aad_check = true
+    }
+    "app_kv_usr_rimaz" = {
+      "principal_id"                   = "#{local_user_id}#"
+      "roles"                          = ["Owner", "Key Vault Secrets Officer"]
+      skip_service_principal_aad_check = false
+    }
+  }
+}
